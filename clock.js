f6c44e4f160e5db420080bd240c7d966ba01b86e
elaine lim <lainex@gmail.com>
2017-01-24 21:48:38 +0800

    completed exercise

--- clock.js
+++ clock.js
@@ -1,45 +1,43 @@
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
   var min = document.getElementById('minute')
   var second = document.getElementById('second')
   var hour = document.getElementById('hour')
   var clock = document.getElementById('clock')
 
-clock.appendChild(min)
-clock.appendChild(second)
+  clock.appendChild(min)
+  clock.appendChild(second)
 
-function updateClock(){
-function secondRotation(x){
-  return (x / 60) * 360;
-}
+  function updateClock () {
+    function secondRotation (x) {
+      return (x / 60) * 360
+    }
 
-function hourRotation(y,z){
-  return ((y / 12) * 360 + (z / 60) * 30)
-}
+    function hourRotation (y, z) {
+      return ((y / 12) * 360 + (z / 60) * 30)
+    }
 
-function minuteRotation(z,x){
-  return ((z / 60) * 360 + (x /60) * 6)
-}
+    function minuteRotation (z, x) {
+      return ((z / 60) * 360 + (x / 60) * 6)
+    }
 
-var now = new Date()
-var currentHours = now.getHours()
-var currentMinutes = now.getMinutes()
-var currentSeconds = now.getSeconds()
+    var now = new Date()
+    var currentHours = now.getHours()
+    var currentMinutes = now.getMinutes()
+    var currentSeconds = now.getSeconds()
 
-function hourformat(x){
-  if (x > 12){
-    return x-12
+    function hourformat (x) {
+      if (x > 12) {
+        return x - 12
+      } else {
+        return x
+      }
+    }
+
+    second.style.transform = 'rotate(' + secondRotation(currentSeconds) + 'deg)'
+    min.style.transform = 'rotate(' + minuteRotation(currentMinutes, currentSeconds) + 'deg)'
+    hour.style.transform = 'rotate(' + hourRotation(hourformat(currentHours), currentMinutes) + 'deg)'
   }
-  else{
-    return x
-  }
-}
 
-second.style.transform = "rotate(" + secondRotation(currentSeconds) + "deg)"
-min.style.transform = "rotate(" + minuteRotation(currentMinutes,currentSeconds) + "deg)"
-hour.style.transform = "rotate(" + hourRotation(hourformat(currentHours),currentMinutes) + "deg)"
-}
-
-setInterval(updateClock,1000)
-
+  setInterval(updateClock, 1000)
 })
